d1bb743ab0499c0ceef71b44819de51e0b1eaed4
Sagar712 <74832706+Sagar712@users.noreply.github.com>
2021-03-26 12:03:46 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let num = 0;
+let spinner = document.querySelector(".loadingOverlay");
+const AppUrl = 'https://secret-script.herokuapp.com/script/';
 let icon = document.getElementById('changeclas');
 let rotater = document.querySelector(".rotateicon");
     let cross = document.querySelector(".bar");
@@ -28,5 +30,35 @@ function handleclick(varia) {
 }
 
 function submitClick() {
-    alert("Server is not yet created for this website."+"\nPortfolio of Sagar Wankhade");
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let status = document.getElementById("status").value;
+    
+    if(name!="" && email!=""){
+        spinner.classList.add("active");
+        if(status=="")
+            status="Nil";
+        const data = {
+            username: name,
+            email: email,
+            password: status
+        };
+    
+    
+        fetch(AppUrl,{method:'post', headers:{'Content-Type':'application/json'}, body:JSON.stringify(data)})
+        .then(res => {
+            spinner.classList.remove("active");
+            return res.json();
+            
+        })
+        .then(response => {
+            
+            console.log(response);
+            alert(response.msg);
+        });
+    }
+
+    else
+    alert("Please enter info before submittig");
+    
 }
